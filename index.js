a86a22f60d4623755e5bdc07d5ec2b88b4f1db1f
UP2010416 <91854274+UP2010416@users.noreply.github.com>
2022-02-22 00:06:35 +0000

    worksheet

--- index.js
+++ index.js
@@ -6,3 +6,46 @@
  */
 
 'use strict';
+async function showMessage(elem,url){
+    const response = await fetch(url)
+    const text = await response.text()
+    elem.textContent=text
+}
+
+async function showList(elem,url){
+    const response = await fetch(url)
+    const data = await response.json()
+    data.forEach((item=>{
+        let listitem = document.createElement("li")
+        listitem.textContent=item
+        elem.appendChild(listitem)
+    }))
+}
+
+function startShowingMessage(elem, url){
+    setInterval(async function(){
+      const response = await fetch(url)
+      const text = await response.text()
+      elem.textContent = text
+    }, 1000)
+  }
+
+async function handleError(elem,url){
+    const response = await fetch(url)
+    if(response.ok){
+        const text = await response.text()
+        elem.textContent=text
+    } else{
+        elem.textContent='OH DEAR'
+    }
+    
+}
+
+function drawBox(canvas,url){
+    setInterval(async function(){
+        const response = await fetch(url)
+        const text = await response.json()
+        let ctx = canvas.getContext('2d')
+        ctx.fillRect(text.x,text.y,10,10)
+    }, 1000)
+}
